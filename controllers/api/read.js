78b06365a01e5c5b98ccdf166dd555d3ff6a4db4
DarioJavierCamacho <84812682+DarioJavierCamacho@users.noreply.github.com>
2023-08-22 18:54:58 -0300

    fixed read all

--- controllers/api/read.js
+++ controllers/api/read.js
@@ -2,10 +2,11 @@ import city from "../../models/City.js";
 
 export default async (req, res, next)=>{
     try{
-        let queries = {};
+       
+        let mySort = {city: 1}
         //if(req.query.id) queries.id = new RegExp(req.query.id,'i');
         //if(req.query.country) queries.country = new RegExp(req.query.country,'i');
-        let found = await city.find(queries);
+        let found = await city.find().sort(mySort);//ordenados alfabeticamente por nombre de ciudad
         return res.status(200).json({cities:found,sucess:true});
     }
     catch(error){
