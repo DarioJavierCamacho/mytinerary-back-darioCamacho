78919414dfe5a9d18818ae39b5bc960d76f7b15f
DarioJavierCamacho <84812682+DarioJavierCamacho@users.noreply.github.com>
2023-08-22 12:26:48 -0300

    crud sprint2

--- controllers/api/read.js
+++ controllers/api/read.js
@@ -4,14 +4,12 @@ export default async (req, res, next)=>{
     try{
         let queries = {};
         //if(req.query.id) queries.id = new RegExp(req.query.id,'i');
-        if(req.query.country) queries.country = new RegExp(req.query.country,'i');
+        //if(req.query.country) queries.country = new RegExp(req.query.country,'i');
         let found = await city.find(queries);
-        return res.status(200).json({cities:found});
+        return res.status(200).json({cities:found,sucess:true});
     }
     catch(error){
         console.log(error);
         next(error);
     }
 }
-
-
