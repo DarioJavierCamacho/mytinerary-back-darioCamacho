943a865c5249129e903dac3ac58212b4fcd71061
DarioJavierCamacho <84812682+DarioJavierCamacho@users.noreply.github.com>
2023-08-31 22:21:02 -0300

    finished itineraries controller and router

--- controllers/city/read.js
+++ controllers/city/read.js
@@ -3,8 +3,7 @@ import city from "../../models/City.js";
 export default async (req, res, next)=>{
     try{
         let mySort = {city: 1}
-        let found = await city.find().sort(mySort).populate({path: 'itineraries',
-        select : 'name usrImg -_id'});//ordenados alfabeticamente por nombre de ciudad
+        let found = await city.find().sort(mySort);//ordenados alfabeticamente por nombre de ciudad
         return res.status(200).json({cities:found,sucess:true});
     }
     catch(error){
